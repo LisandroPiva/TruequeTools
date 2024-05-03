79552c23f52736d345c24ced4bcaf8509af445fb
Thiago Gauna Rubio <thiagogaunarubio@gmail.com>
2024-05-03 20:03:28 -0300

    aaaa

--- apptrueques/models.py
+++ apptrueques/models.py
@@ -9,54 +9,55 @@ class Sucursal(models.Model):
 
 
 
-
-
 class UsuarioManager(BaseUserManager):
-    # Administrador personalizado para el modelo Usuario
-
     def create_user(self, email, password=None, **extra_fields):
-        # Método para crear un usuario regular
         if not email:
-            raise ValueError('El email es obligatorio.') # Se asegura de que se proporcione un email
+            raise ValueError('El email es obligatorio.')
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields) # Crea un nuevo usuario con el email normalizado y otros campos extra
-        user.set_password(password) # Establece la contraseña del usuario
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
-        # Método para crear un superusuario
-        extra_fields.setdefault('is_staff', True) # Establece is_staff en True
-        extra_fields.setdefault('is_superuser', True) # Establece is_superuser en True
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
         if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser debe tener is_staff=True.') # Se asegura de que is_staff sea True para superusuarios
+            raise ValueError('Superuser debe tener is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser debe tener is_superuser=True.') # Se asegura de que is_superuser sea True para superusuarios
+            raise ValueError('Superuser debe tener is_superuser=True.')
 
         return self.create_user(email, password, **extra_fields)
 
 class Usuario(AbstractBaseUser):
-    # Definición del modelo Usuario
+    email = models.EmailField(unique=True)
+    nombre = models.CharField(max_length=100)
+    reputacion = models.IntegerField(null=True)
+    fecha_de_nacimiento = models.DateField(auto_now_add=True)
+    sucursal_favorita = models.ForeignKey(Sucursal, related_name="usuarios", null=True, on_delete=models.SET_NULL)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-    # Campos del modelo
-    email = models.EmailField(unique=True) # Correo electrónico del usuario
-    nombre = models.CharField(max_length=100) # Nombre del usuario
-    reputacion = models.IntegerField(null=True) # Reputación del usuario
-    fecha_de_nacimiento = models.DateField(auto_now_add=True) # Fecha de nacimiento del usuario
-    sucursal_favorita = models.ForeignKey(Sucursal, related_name="usuarios", null=True, on_delete=models.SET_NULL) # Sucursal favorita del usuario
-    is_active = models.BooleanField(default=True) # Indica si el usuario está activo
-    is_staff = models.BooleanField(default=False) # Indica si el usuario es miembro del personal administrativo
+    objects = UsuarioManager()
 
-    objects = UsuarioManager() # Administrador del modelo Usuario
-
-    USERNAME_FIELD = 'email' # Campo utilizado para identificar al usuario durante el inicio de sesión
-    REQUIRED_FIELDS = ['nombre'] # Campos requeridos para crear un usuario
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nombre']
 
     def __str__(self):
-        # Método que retorna una representación en cadena del objeto Usuario
         return self.email
 
+'''
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+    reputacion = models.IntegerField(null=True)
+    fecha_de_nacimiento = models.DateField(auto_now_add=True)
+    sucursal_favorita = models.ForeignKey(Sucursal, related_name="usuarios", null=True, on_delete=models.SET_NULL)
+    def __str__(self):
+        return self.nombre
+'''
 class Publicacion(models.Model):
     usuario_propietario = models.ForeignKey(Usuario, related_name="publicaciones", on_delete=models.CASCADE)
     titulo = models.CharField(max_length=100)
