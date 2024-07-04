34855e4ceda4674289407d92a329d295f93d0fe6
GeronimoErrante <geronimoerrante7@gmail.com>
2024-07-03 23:39:32 -0300

    agrego marca de borrado sucursal y logica de asignacion de nueva sucursal a empleados, agrego editar perfil (falta testear)

--- apptrueques/models.py
+++ apptrueques/models.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 class Sucursal(models.Model):
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
+    borrada = models.BooleanField(default=False)
     def __str__(self):
         return self.nombre
 
